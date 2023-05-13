8b2cf533d8abfdb7f84891aa8ff5bc7cfd597977
Savindushehan <savindushehansamaraweera@gmail.com>
2023-05-13 13:01:30 +0530

    upload all

---
+++ JS/javascript.js
@@ -0,0 +1,10 @@
+
+
+var btn=document.getElementById("one");
+var frow=document.getElementById("row");
+var fone=document.getElementById("one");
+
+btn.onclick=function(){
+    frow.style.display="block";
+    fone.style.backgroundColor=" hsl(217, 19%, 35%)";
+}
